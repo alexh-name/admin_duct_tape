d9b01174b49ccb2dfb251c6b6643da7f5743b585
alexh-name <1@alexh.name>
2016-07-24 22:26:57 +0200

    roll-passphrase.sh: more explaining

--- roll-passphrase.sh
+++ roll-passphrase.sh
@@ -12,6 +12,26 @@
 
 # Use cases can be generating easier to remember, yet reasonably safe
 # pass phrases.
+#
+# Calculating the entropy assuming the method of generation is known:
+# Taking a dictionary of 399370 english words gives us 399370^4 =
+# 25,439,100,624,082,329,610,000 possible phrases which result in
+# log2(25439100624082329610000) ≈ 74.43 entropy bits for such a phrase.
+# Assuming hardware capable of guessing 350 billion/s [1], it would take
+# ~ 2304 years to try all possible phrases.
+#
+# Calculating the entropy assuming the method of generation is not known:
+# We basically have the symbol set of the case sensitive Latin alphabet
+# (a-z, A-Z) which are 52 distinct symbols. Our phrases have by default at least
+# 30 characters. We get at minimum 52^30 =
+# 3,020,649,319,540,279,026,721,308,472,064,611,280,212,898,898,509,824
+# combinations which result in
+# log2(3020649319540279026721308472064611280212898898509824) ≈ 117 entropy bits.
+# Assuming hardware capable of guessing 350 billion/s [1], it would take
+# ~ 273,669,033,081,492,265,231,690,627,678,536 years not taking into account
+# rainbow tables etc.
+#
+# [1] http://arstechnica.com/security/2012/12/25-gpu-cluster-cracks-every-standard-windows-password-in-6-hours/
 
 # Keep in mind that /usr/share/dict/words can contain offensive words.
 # If you'd like to filter possibly offensive words out of it, this is one
@@ -20,6 +40,8 @@
 # http://www.cs.cmu.edu/~biglou/resources/bad-words.txt, then:
 # grep -iv -f bad-words.txt /usr/share/dict/words > good-words.txt
 # This can consume a lot of RAM.
+
+# Alex H. - https://keybase.io/alexh_name - 2016
 
 set -eu
 
