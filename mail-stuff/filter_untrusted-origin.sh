b0548f81e966f40c78179dadf7505a31cdc233b6
alexh-name <1@alexh.name>
2016-10-14 12:23:08 +0200

    tiding up

--- mail-stuff/filter_untrusted-origin.sh
+++ mail-stuff/filter_untrusted-origin.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
+# Use this in a .qmail file to check whether a mail's last origin was a trusted
+# one. If it was, the next command in .qmail will be executed. If not, the mail
+# won't be processed any further.
+# e.g.:
+# | /usr/local/bin/filter_untrusted-origin
+# | /usr/local/bin/split_multimail
+# ./Maildir/
+
+set -eu
+
 INPUT="$(</dev/stdin)"
 
-grep -F 'Received:' <<<"${INPUT}" \
-| head -n1 \
-| grep -E "^Received: \(qmail [0-9]+ invoked by alias\)" > /dev/null 2>&1
+TRUSTED_ORIGIN_FILE="$1"
+TRUSTED_ORIGIN="$(<${TRUSTED_ORIGIN_FILE})"
 
-if [[ $? -eq 0 ]]; then
+LAST_REC="$(grep -F 'Received:' <<<"${INPUT}" \
+| head -n1)"
+
+if grep -E "^Received: \(${TRUSTED_ORIGIN}\)" <<<"${LAST_REC}" \
+> /dev/null 2>&1; then
   exit 0
  else
   # so qmail will not do any further deliveries in .qmail file
