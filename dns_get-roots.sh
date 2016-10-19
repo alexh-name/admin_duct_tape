31c2d13fde5287363fee81c3edd5badbe725b94a
alexh-name <1@alexh.name>
2016-10-19 20:24:14 +0200

    dns_get-roots.sh: fix $PATH issue

--- dns_get-roots.sh
+++ dns_get-roots.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+dnsip="/usr/local/bin/dnsip"
+dnsqr="/usr/local/bin/dnsqr"
+svc="/usr/local/bin/svc"
+
 mv /etc/dnsroots.global /etc/dnsroot.global.old
-dnsip $(dnsqr ns . | awk '/answer:/ { print $5; }' |sort) \
+${dnsip} $( ${dnsqr} ns . | awk '/answer:/ { print $5; }' | sort ) \
   > /etc/dnsroots.global
 cp /etc/dnsroots.global /service/dnscache/root/servers/@
-svc -du /service/dnscache
-
+${svc} -du /service/dnscache
