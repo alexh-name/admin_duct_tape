39dfa4c1266d482e41f08b9a07bba2d9c4f4ca6f
alexh-name <1@alexh.name>
2016-10-19 20:25:44 +0200

    split_multimail: add forwarded to address saving and message appendix

--- mail-stuff/split_multimail.sh
+++ mail-stuff/split_multimail.sh
@@ -3,9 +3,13 @@
 # Split multiple complete mails (including headers) that are encapsuled into one
 # and forward them to recipients defined in each single mail's "To" field.
 # Form feed is our delimiter here.
-# If any argument is given, the multimail is considered base64 encoded.
 # Usage e.g. in .qmail:
-# | /usr/local/bin/split_multimail [d]
+# | /usr/local/bin/split_multimail [-d] [-t TO_FILE] [-m MSG_FILE]
+# -d: The multimail is considered base64 encoded.
+# -t TO_FILE: All addresses we forwarded to are saved to TO_FILE
+#             (TO_FILE must be writable by user owning .qmail.)
+# -m MSG_FILE: Append text to each forwarded mail contained in MSG_FILE.
+#             (MSG_FILE must be readable by user owning .qmail.)
 
 set -eu
 
@@ -14,12 +18,28 @@ FORWARDBIN=/var/qmail/bin/forward
 # mess822 822field location
 M822FIELD=/usr/local/bin/822field
 
-ARGS_N=$#
+DECODE=''
+TO_LOG=''
+TO_FILE=''
+APPEND=''
+MSG_FILE=''
+
+while getopts dt:m: name; do
+  case $name in
+    d)  DECODE=1;;
+    t)  TO_LOG=1
+        TO_FILE="$OPTARG";;
+    m)  APPEND=1
+        MSG_FILE="$OPTARG";;
+    ?)  exit 2;;
+  esac
+done
+
 
 # Concatenated and by form feed delimited mails
 MAILS="$( awk 'BEGIN { RS = "\n\n" }; NR=="2"' </dev/stdin )"
 
-if [[ ${ARGS_N} -ne 0 ]]; then
+if [[ ${DECODE} -eq 1 ]]; then
   MAILS="$( base64 -d <<<"${MAILS}" )"
 fi
 
@@ -48,8 +68,16 @@ function forward {
   until [[ ${n} -gt ${N_R} ]]; do
     # arrays start at 0
     array_n=$(( ${n} - 2 ))
-    to="$( ${M822FIELD} To <<<"${array[${array_n}]}" )"
-    ${FORWARDBIN} ${to} <<<"${array[${array_n}]}"
+    msg="${array[${array_n}]}"
+    if [[ ${APPEND} -eq 1 ]]; then
+      appendix="$( <"${MSG_FILE}" )"
+      msg="${msg} ${appendix}"
+    fi
+    to="$( ${M822FIELD} To <<<"${msg}" )"
+    if [[ ${TO_LOG} -eq 1 ]]; then
+      echo ${to} >> "${TO_FILE}"
+    fi
+    ${FORWARDBIN} ${to} <<<"${msg}"
     n=$(( ${n} + 1 ))
   done
 }
