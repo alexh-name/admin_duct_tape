e53b94f689b6bf2d4313bb4ce74b9eb4c25448d3
alexh-name <1@alexh.name>
2016-12-18 15:38:34 +0100

    routine/queue: check for age of msg

--- routine/queue.sh
+++ routine/queue.sh
@@ -7,17 +7,32 @@ problem=''
 problem_msg='
 --- smth stuck in queue ---'
 
-out=$( /usr/local/bin/qmailctl stat | grep 'messages in queue:' )
-out_part=$( rev <<<"${out}" | cut -f 1 -d ' ' )
+out=$(
+  find /var/qmail/queue/mess/ -type f \
+  | while read msg; do
+    msg_n="$( rev <<<"${msg}" | cut -d '/' -f 1 | rev )"
+    birth="$( stat --printf="%Y\n" "${msg}" )"
+    birth_f="$( stat --printf="%y\n" "${msg}" )"
+    date="$( date +%s )"
+    age="$(( ${date} - ${birth} ))"
+    if (( "${age}" > '3600' )); then
+      status='alarming'
+    else
+      status='fine'
+    fi
+    printf "msg %s: %s - %s" "${msg_n}" "${birth_f}" "${status}"
+  done
+ )
+out_part="$( rev <<<"${out}" | cut -f 1 -d ' ' | rev )"
 
-if (( "${out_part}" != 0 )) ; then
+if grep -F 'alarming' <<<"${out_part}" > /dev/null; then
   out="${out}${problem_msg}"
-  problem=1
+  problem='1'
 fi
 
 echo "${out}"
 
-if [[ ${problem} -eq 1 ]]; then
+if [[ "${problem}" -eq '1' ]]; then
  exit 10
 else
  exit 0
