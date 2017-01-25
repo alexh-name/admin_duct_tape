7835281b0b40aebf2af8e679d984f8ceee8badc1
alexh-name <1@alexh.name>
2017-01-25 21:03:41 +0100

    routine: style

--- routine/_routine.sh
+++ routine/_routine.sh
@@ -6,20 +6,20 @@ SCRIPTS="${dir}/space.sh
 ${dir}/load.sh
 ${dir}/queue.sh"
 
-V=$1
+V="$1"
 
 function fire {
   script=$1
   v=$2
-  output="$( nice -n 10 mksh ${script} )"
-  if [[ ! $? -eq 0 ]] || [[ ! -z ${v} ]]; then
+  output="$( nice -n 10 mksh "${script}" )"
+  if (( "$?" != '0' )) || [ ! -z "${v}" ]; then
     echo "${output}"
   fi
 }
 
 function cycle {
   while read script; do
-    fire $script $V
+    fire "${script}" "${V}"
   done <<<"${SCRIPTS}"
 }
 
