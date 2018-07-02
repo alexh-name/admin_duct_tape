1329c9fece511f9b36e6889e95a6b1193a71b26f
alexh <git@alexh.name>
2018-07-02 21:17:47 +0200

    update/parity.sh: sha256 sums are provided now

--- update/parity.sh
+++ update/parity.sh
@@ -26,8 +26,8 @@ function parity_bin {
     | sed -e 's/^\[parity\](//' -e 's/).*$//' -e 's/^http:/https:/'
   )"
 
-  # The list provides a md5sum, ugh.
-  PARITY_BIN_MD5_LIST="$(
+  # The list provides a SHA256sum.
+  PARITY_BIN_SHA256_LIST="$(
     echo "${PARITY_BIN}" \
     | awk '{print $7}'
   )"
@@ -39,46 +39,19 @@ function parity_bin {
   fi
 }
 
-function parity_md5 {
-  echo "${PARITY_BIN_MD5_LIST}  parity" > parity.md5
-  md5sum -c parity.md5
-}
-
-function parity_hash {
-  # Basically the same as in parity_bin
-  # sha256 hash exists, but in a separate file
-  PARITY_HASH="$(
-    echo "${LIST}" \
-    | egrep '^linux \| x86_64 \| \[parity\.sha256\]'
-  )"
-  # Again, https works!
-  PARITY_HASH_URL="$(
-    echo "${PARITY_HASH}" \
-    | awk '{print $5}' \
-    | sed -e 's/^\[parity\.sha256\](//' -e 's/).*$//' -e 's/^http:/https:/'
-  )"
-  if [ -e 'parity.256' ]; then
-    echo 'parity.256 exists.'
-  else
-    wget "${PARITY_HASH_URL}" -O parity.256
-  fi
-
-  # Replace path in sha256 file to match our local file
-  sed -i 's/target\/x86_64-unknown-linux-gnu\/release\/parity$/parity/' parity.256
-  sha256sum -c parity.256
+function parity_sha256 {
+  echo "${PARITY_BIN_SHA256_LIST}  parity" > parity.sha256
+  sha256sum -c parity.sha256
 }
 
 echo '  ## Getting version list...'
 parity_list
 echo '  ## Getting parity binary...'
 parity_bin
-echo '  ## Checking md5 hash from list...'
-parity_md5
-echo '  ## Getting and checking sha256 hash from file...'
-parity_hash
+echo '  ## Checking sha256 hash from list...'
+parity_sha256
 
 echo '  ## All fine, cleaning up and putting parity to /usr/local/bin/'
-rm parity.md5
-rm parity.256
+rm parity.sha256
 install parity /usr/local/bin/
 
